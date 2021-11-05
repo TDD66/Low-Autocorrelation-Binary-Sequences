0a7604871366a83cfb13da9b4809747316303cc8
TDD66 <79865373+TDD66@users.noreply.github.com>
2021-11-05 16:03:12 +0000

    Update LABS.cpp

--- LABS.cpp
+++ LABS.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 
+
 float EnergyCalc(int, float*);
 float MeritCalc(int, float);
-void printSequence(float*, int);
-void generateBinarySequences(int, float*, int);
+void printSequence(int, float*);
+float Autocorrelation(int);
+float g;
 
 int main()
 {
-  int l = 3;  
-  float size[l]; 
-  generateBinarySequences(l, size, 0);  
-
-
+  int l = 18;   
+ // printf("Enter length of sequence: ");
+  //scanf("%d", &l);
+printf("Length of sequence: %d\n", l);
+  Autocorrelation(l); 
 
 	return 0;
 }
@@ -43,7 +48,7 @@ float EnergyCalc(int length, float sequence[]) {
         energy += Y[x];
 	}
     
-    cout<<" "<<energy;
+  //  cout<<" "<<energy;
     
     return energy;
 
@@ -52,12 +57,11 @@ float EnergyCalc(int length, float sequence[]) {
 float MeritCalc(int length, float energy){
 
     float meritfactor = (length*length)/(2*energy);
-    cout<<" \n"<<meritfactor;
     return meritfactor;
 
 }
 
-void printSequence(float sequence[], int length)
+void printSequence(int length, float sequence[])
 {
 	for (int i = 0; i < length; i++) {
 		cout << sequence[i] << " ";
@@ -65,17 +69,82 @@ void printSequence(float sequence[], int length)
 	cout << "\n";
 }
 
-void generateBinarySequences(int length, float sequence[], int z)
-{
-	 if (z == length) {
-		printSequence(sequence, length);
-		return;
-	} 
-	sequence[z] = -1;
-	generateBinarySequences(length, sequence, z + 1);
+float Autocorrelation(int length) {
+	
+int power = (pow(2, length));
+float sequence[length];
+float energy[power];
+float meritfactor[power];
+float minenergy = 1000;
+float maxfactor;
+vector<int> optimal{0};
+int num_of_optimal = 1;
 
-	sequence[z] = 1;
-	generateBinarySequences(length, sequence, z + 1);
-    
+for (int count = 0b0; count < power; count++){
+
+	for (int i = 0; i < length; i++){
+		sequence[i] = (count >> i) & 1;
+
+		if(sequence[i]==0){
+			sequence[i] = -1;
+		}
+
+	}
+	//printSequence(length, sequence);
+	energy[count] = EnergyCalc(length, sequence);
+	meritfactor[count] = MeritCalc(length, energy[count]);
+
+	 if(energy[count] < minenergy){
+		minenergy = energy[count];
+		optimal[0] = count;
+		
+	} else if(energy[count] == minenergy){
+		optimal.push_back(count);
+		num_of_optimal++;
+	} 
+
+	}
+	float display[num_of_optimal];
+	maxfactor = MeritCalc(length, minenergy);
+	//printSequence(power, energy);
+	//printSequence(power, meritfactor);
+
+	if(num_of_optimal == 1){
+		cout << "One optimal solution\n";
+		printf("Minimum energy = %.0f\n", minenergy);
+		printf("Maximum merit factor = %.3f\n", maxfactor);
+
+		for(int j = 0; j < num_of_optimal; j++){
+
+			for (int k = 0; k < length; k++){
+		display[k] = (optimal[j] >> k) & 1;
+
+		if(display[k]==0){
+			display[k] = -1;
+		}
+
+		}
+		printSequence(length, display);
+		}	
+	} else{
+		cout << num_of_optimal << " optimal sequences found\n\n";
+		printf("Minimum energy = %.0f\n", minenergy);
+		printf("Maximum merit factor = %.3f\n", maxfactor);
+		for(int j = 0; j < num_of_optimal; j++){
+
+			for (int k = 0; k < length; k++){
+		display[k] = (optimal[j] >> k) & 1;
+
+		if(display[k]==0){
+			display[k] = -1;
+		}
+
+		}
+		printSequence(length, display);
+	} 
+
+	cout << "\nEND";
+
+	return 0;
 }
-    
+}
